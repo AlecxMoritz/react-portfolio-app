e042855008126155722d18dd16a4563c9e9e24a0
Alecx Moritz <alecxmoritz7@gmail.com>
2019-05-24 14:35:23 -0400

    added sitebar & footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Sitebar from './Components/UI/Sitebar';
+import Footer from './Components/UI/Footer';
 
 function App() {
   return (
     <div>
-     
+     <Sitebar />
+     <Footer />
     </div>
   );
 }
--- src/Components/UI/Footer.js
+++ src/Components/UI/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Radium from 'radium';
+
+const styles = {
+    footer : {
+        backgroundColor : 'purple',
+        color : 'white',
+        position : 'absolute',
+        bottom : 0,
+        left : 0,
+        right : 0,
+        height : '10vh'
+    }
+}
+
 
 const Footer = () => {
     return (
-        <footer>
+        <footer style={styles.footer}>
             <p>footer</p>
         </footer>
     )
 };
 
-export default Footer;
+export default Radium(Footer);
